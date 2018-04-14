38ad3363ab1b67f9360e6f4694fae15e311de361
Petko Kostadinov <petkoxray@gmail.com>
2018-04-14 22:27:13 +0300

    more solutions

---
+++ Functions, Triggers and Transactions/Lab/3.sql
@@ -0,0 +1,14 @@
+DELIMITER $$
+CREATE PROCEDURE usp_raise_salary_by_id(id INTEGER(11))
+BEGIN
+	START TRANSACTION;
+	IF((SELECT count(employee_id) FROM employees WHERE employee_id like id)<>1) THEN
+	ROLLBACK;
+	ELSE
+		UPDATE employees AS e SET salary = salary + salary*0.05 
+		WHERE e.employee_id = id;
+	END IF;  
+END $$
+DELIMITER ;
+
+CALL usp_raise_salary_by_id(178);
